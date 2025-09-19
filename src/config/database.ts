cd8e7f01fee9b3f33460045fba21a6690a616e2d
Flamarionfp <flamarionpinto@sou.faccat.br>
2025-09-18 21:51:44 -0300

    chore: add log db connect

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,21 @@
-import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+import fs from "fs";
+import path from "path";
 
 export async function connectDatabase() {
+  const dbPath = process.env.DATABASE_PATH ?? "";
+
+  if (!fs.existsSync(dbPath)) {
+    console.error("Banco de dados não encontrado em:", dbPath);
+
+    throw new Error("Banco de dados não encontrado");
+  }
+
+  console.log("Conectando ao banco de dados em:", dbPath);
+
   return open({
-    filename: `${process.env.DATABASE_PATH}`,
+    filename: dbPath,
     driver: sqlite3.Database,
   });
 }
