69ae7bdb06f2d8d4914e715d91c0bf98c25660ce
Flamarionfp <flamarionpinto@sou.faccat.br>
2025-09-25 20:44:05 -0300

    chore: change swagger route

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(morgan("dev"));
 
